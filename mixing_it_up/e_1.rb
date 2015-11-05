b225f88944b74038fe2798b3cd190a2f79bbc3c1
Kyle Conway <kyleconway@rocketmail.com>
2015-11-05 12:10:51 -0800

    Chapter 5

---
+++ mixing_it_up/e_1.rb
@@ -0,0 +1,9 @@
+puts "What is your first name?"
+f_name = gets.chomp.capitalize
+puts "What is your middle name?"
+m_name = gets.chomp.capitalize
+puts "What is your last name?"
+l_name = gets.chomp.capitalize
+
+puts " "
+puts "Pleased to meet you #{f_name} #{m_name} #{l_name}!!!"
---
+++ mixing_it_up/e_2.rb
@@ -0,0 +1,7 @@
+puts "What is your favorite number?"
+answer = gets.chomp.to_i
+
+better_answer = answer + 1
+
+puts " "
+puts "#{answer} is a pretty okay number...#{better_answer} is better though..."
