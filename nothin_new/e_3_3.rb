72b88d58503d439f411ca81e3c86c562367433be
Kyle Conway <kyleconway@rocketmail.com>
2015-11-24 12:34:30 -0800

    Chapter 10

---
+++ nothin_new/e_3_3.rb
@@ -0,0 +1,13 @@
+# The well-known quicksort algorithm
+
+def sort arr
+  return arr if arr.length <= 1
+
+  middle  = arr.pop
+  less    = arr.select{|x| x.downcase <  middle.downcase}
+  more    = arr.select{|x| x.downcase >= middle.downcase}
+
+  sort(less) + [middle] + sort(more)
+end
+
+puts(sort(['can', 'feel', 'singing', 'like', 'a', 'can']).join(' '))
